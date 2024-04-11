9a712b42956893474fc0bd1058b1a0be9606514b
profstein <profstein@users.noreply.github.com>
2024-04-11 14:38:25 +0000

    change output to _site

--- .eleventy.js
+++ .eleventy.js
@@ -11,7 +11,7 @@ module.exports = function(eleventyConfig) {
       markdownTemplateEngine: 'njk',
       dir: {
         input: "src",
-        output: "public"
+        output: "_site"
       }
     }
 };
